fad73b9867e2b7369a7bdcc135eec64679342409
number000000 <maggieliulmh@gmail.com>
2024-09-12 16:15:09 -0400

    implemented inheritance SortedIntList

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,31 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int totalAdded;
 
+    public InheritanceSortedIntList(){
+        super();
+        this.totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int num){
+        boolean result = super.add(num);
+        if(result){
+            this.totalAdded++;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list){
+        boolean result = super.addAll(list);
+        return result;
+    }
+
+    public int getTotalAdded(){
+        return this.totalAdded;
+    }
 }
